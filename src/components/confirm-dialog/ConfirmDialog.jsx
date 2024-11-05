c058040f63b52d625b4f3c630fe0cb919790305a
aria-sharifnia <aria.sharifnia@gmail.com>
2024-11-05 15:52:32 -0700

    finishing up the comfrim dialog thing

--- src/components/confirm-dialog/ConfirmDialog.jsx
+++ src/components/confirm-dialog/ConfirmDialog.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Stack } from '@mui/material';
+import { Dialog, DialogContent, Stack } from '@mui/material';
 import WarningIcon from '@mui/icons-material/Warning';
 import './ConfirmDialog.css';
 
 function ConfirmDialog({ open, onClose, onConfirm }) {
   return (
-    <Dialog open={open} onClose={onClose} classes={{ paper: 'confirm-dialog' }}>
+    <Dialog open={open} onClose={onClose} PaperProps={{ sx: { borderRadius: '15px' } }}>
       <DialogContent className="dialog-content">
-        <WarningIcon className="icon" />
-        <Typography className="title">
-        Are you sure you want to remove this photograph from your cart?
-        </Typography>
-      </DialogContent>
-      <DialogActions>
+        <WarningIcon fontSize="large"className="icon"/>
+        <span className="title">
+            Are you sure you want to remove this photograph from your cart?
+        </span>
         <Stack className="button-container"direction={"row"}>
-        <Button onClick={onClose}className="deny-button">
+        <button onClick={onClose}className="deny-button">
           Cancel
-        </Button>
-        <Button onClick={onConfirm} className="confirm-button">
+        </button>
+        <button onClick={onConfirm} className="confirm-button">
           Remove
-        </Button>
+        </button>
         </Stack>
-      </DialogActions>
+      </DialogContent>
     </Dialog>
   );
 }
