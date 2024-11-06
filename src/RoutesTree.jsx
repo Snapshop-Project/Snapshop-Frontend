0766dffa5a2dc6c6642a6d54d5ac1c50e348589f
HQ <hasan.qasim@ucalgary.ca>
2024-11-05 23:49:27 -0600

    Added my part
    tweaked picture page and photogrid mostly

--- src/RoutesTree.jsx
+++ src/RoutesTree.jsx
@@ -1,20 +1,18 @@
-import React, { Fragment } from "react";
+// src/RoutesTree.js
+import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Browser from './pages/browsing/index';
 import Cart from "./pages/cart";
+import PicturePage from "./pages/PicturePage"; // Make sure the path is correct
 
 const RoutesTree = () => {
   return (
-    <div>
-      <Routes>
-        <Fragment>
-            <Route path="/" element={<Browser />}/>
-            <Route path="/cart" element={<Cart />}/>
-        </Fragment>
-      </Routes>
-    </div>
-
-  )
+    <Routes>
+      <Route path="/" element={<Browser />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/picture/:id" element={<PicturePage />} /> {/* Route for PicturePage */}
+    </Routes>
+  );
 }
 
 export default RoutesTree;
